7cddfacd1af47a494c640d865d95ebebd39ea79b
faizanibneadil <107530101+faizanibneadil@users.noreply.github.com>
2023-05-30 01:22:08 +0500

    fix - layout sidebar and bottom bar fixes

--- src/app/(admin)/components/layouts/sideMenuBar.tsx
+++ src/app/(admin)/components/layouts/sideMenuBar.tsx
@@ -32,29 +32,29 @@ const ListItem = ({ children, title, ...props }: any) => (
 export default function SideMenuBar() {
   return (
     <NavigationMenu.Root className="">
-      <NavigationMenu.List className="flex flex-row justify-center md:space-x-0 md:space-y-2 md:flex-col">
+      <NavigationMenu.List className="flex flex-row justify-center space-x-2 md:space-x-0 md:space-y-2 md:flex-col">
         <NavigationMenu.Item className="cursor-pointer md:px-6 hover:bg-gray-500">
-            <Link href="/admin" className="flex flex-col items-center space-y-2 md:flex-row md:space-x-2 md:space-y-0">
-                <div className="p-4 md:p-0"><PieChartIcon /></div>
-                <div className="hidden md:block">Dashboard</div>
+            <Link href="/admin" className="flex flex-col items-center p-2 md:p-0.5 md:flex-row md:space-x-2 md:space-y-0">
+                <div className="p- md:p-0"><PieChartIcon className="w-5 h-5 md:w-4 md:h-4" /></div>
+                <div className="text-xs md:text-sm line-clamp-1">Dashboard</div>
             </Link>
         </NavigationMenu.Item>
         <NavigationMenu.Item className="cursor-pointer md:px-6 hover:bg-gray-500">
-            <Link href="/create" className="flex flex-col items-center space-y-2 md:flex-row md:space-x-2 md:space-y-0">
-                <div className="p-4 md:p-0"><PlusIcon /></div>
-                <div className="hidden md:block">Create</div>
+            <Link href="/create" className="flex flex-col items-center p-2 md:p-0.5 md:flex-row md:space-x-2 md:space-y-0">
+                <div className="p- md:p-0"><PlusIcon className="w-5 h-5 md:w-4 md:h-4" /></div>
+                <div className="text-xs md:text-sm line-clamp-1">Create</div>
             </Link>
         </NavigationMenu.Item>
         <NavigationMenu.Item className="cursor-pointer md:px-6 hover:bg-gray-500">
-            <Link href="/products" className="flex flex-col items-center space-y-2 md:flex-row md:space-x-2 md:space-y-0">
-                <div className="p-4 md:p-0"><DashboardIcon /></div>
-                <div className="hidden md:block">Products</div>
+            <Link href="/products" className="flex flex-col items-center p-2 md:p-0.5 md:flex-row md:space-x-2 md:space-y-0">
+                <div className="p- md:p-0"><DashboardIcon className="w-5 h-5 md:w-4 md:h-4" /></div>
+                <div className="text-xs md:text-sm line-clamp-1">Products</div>
             </Link>
         </NavigationMenu.Item>
         <NavigationMenu.Item className="cursor-pointer md:px-6 hover:bg-gray-500">
-            <Link href="/settings" className="flex flex-col items-center space-y-2 md:flex-row md:space-x-2 md:space-y-0">
-                <div className="p-4 md:p-0"><GearIcon /></div>
-                <div className="hidden md:block">Settings</div>
+            <Link href="/settings" className="flex flex-col items-center p-2 md:p-0.5 md:flex-row md:space-x-2 md:space-y-0">
+                <div className="p- md:p-0"><GearIcon className="w-5 h-5 md:w-4 md:h-4" /></div>
+                <div className="text-xs md:text-sm line-clamp-1">Settings</div>
             </Link>
         </NavigationMenu.Item>
       </NavigationMenu.List>
--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -6,12 +6,20 @@ import SideMenuBar from "./components/layouts/sideMenuBar";
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div>
+      {/* <div className="text-white bg-base-300">
+        <div className="flex flex-col-reverse items-center md:flex-row md:max-w-min">
+          <div className="sticky bottom-0 w-full bg-base-300">
+            <SideMenuBar />
+          </div>
+          <div className="bg-base-100 md:h-screen md:w-auto md:overflow-x-auto">{children}</div>
+        </div>
+      </div> */}
       <div className="h-full text-white bg-black md:h-screen">
         <div className="flex flex-col-reverse items-center md:h-full md:flex-row">
           <div className="sticky bottom-0 w-full text-center text-white bg-black md:w-auto">
             <SideMenuBar />
           </div>
-          <div className="mx-2 bg-gray-900 md:w-full rounded-b-lg md:overflow-x-auto md:rounded-l-lg md:mx-0 md:rounded-br-none md:h-[calc(100vh-5%)]">
+          <div className="bg-gray-900 md:w-full md:overflow-x-auto  md:h-[calc(100vh)]">
             {children}
           </div>
         </div>
