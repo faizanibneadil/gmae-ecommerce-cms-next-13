5c5cd60a26d657a31d8d6f40c7f6aac9b3636e04
faizanibneadil <107530101+faizanibneadil@users.noreply.github.com>
2023-05-30 19:36:41 +0500

    fix - export default function

--- src/app/components/transitionButton.tsx
+++ src/app/components/transitionButton.tsx
@@ -5,13 +5,23 @@ import { ReactNode, forwardRef, useTransition } from "react";
 interface ButtonProps {
   className?: string;
   path: string;
-  children: ReactNode
+  children: ReactNode;
 }
 
-const TransitionButton = forwardRef<HTMLButtonElement, ButtonProps>(({ className, path, children }, forwardedRef) => {
-    const router = useRouter()
-    const [isPending, startTransition] = useTransition()
-    return <button disabled={isPending} onClick={() => startTransition(() => router.replace(path))} className={`${isPending ? `loading ${className}` : className}`} ref={forwardedRef}>{isPending ? `` : children}</button>;
-});
-
-export default TransitionButton
+export default function TransitionButton({
+  className,
+  path,
+  children,
+}: ButtonProps) {
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+  return (
+    <button
+      disabled={isPending}
+      onClick={() => startTransition(() => router.replace(path))}
+      className={`${isPending ? `loading ${className}` : className}`}
+    >
+      {isPending ? `` : children}
+    </button>
+  );
+}
