712a2ef2f9836149974c540bc7e7d074a3204f8e
faizanibneadil <107530101+faizanibneadil@users.noreply.github.com>
2023-06-02 00:26:52 +0500

    fix

--- src/app/(Customer)/browse/page.tsx
+++ src/app/(Customer)/browse/page.tsx
@@ -5,12 +5,8 @@ export default async function Page() {
   const categories = await prisma.categories.findMany();
   return (
     <div className="w-full p-2 ">
-      <div className="grid grid-cols-2 gap-2 md:grid-cols-4 lg:grid-cols-6">
-        {/* {categories?.map((category) => (
-          <CategoryCard key={category.id} category={category} />
-        ))} */}
-        
-        {categories?.map( c => <CategoryCard key={c.id} category={c} />)}
+      <div className="grid grid-cols-2 gap-2 md:grid-cols-4 lg:grid-cols-6">        
+        {categories?.map(c => <CategoryCard key={c.id} category={c} />)}
       </div>
     </div>
   );
--- src/app/(Customer)/components/categoryCard.tsx
+++ src/app/(Customer)/components/categoryCard.tsx
@@ -1,11 +1,20 @@
 "use client";
 
 import { Categories } from "@prisma/client";
+import { CldImage } from "next-cloudinary";
 
 export default function CategoryCard({ category }: { category: Categories }) {
   return (
-    <div className="w-[15rem] rounded-2xl bg-base-300">
-      <img className="rounded-t-2xl" src={`${category.image}`} />
+    <div className="w-full rounded-2xl bg-base-300">
+      <CldImage
+        width={400}
+        height={400}
+        crop="thumb"
+        gravity="faces"
+        src={`${category.image}`}
+        alt={`${category.name}`}
+      />
+      {/* <img className="rounded-t-2xl" src={`${category.image}`} /> */}
       <div className="px-4 py-2">
         <h5 className="mb-2 tracking-tight text-gray-900 text-md dark:text-white line-clamp-1">
           {category.name}
