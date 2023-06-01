9bfd765d2eb30c38ac3c4c507bcc6e33bc8d836b
faizanibneadil <107530101+faizanibneadil@users.noreply.github.com>
2023-06-01 22:22:29 +0500

    fix

--- src/app/(Customer)/browse/error.tsx
+++ src/app/(Customer)/browse/error.tsx
@@ -1,4 +1,4 @@
-"use client"; // Error components must be Client Components
+"use client"; 
 
 import { useEffect } from "react";
 
@@ -10,7 +10,6 @@ export default function Error({
   reset: () => void;
 }) {
   useEffect(() => {
-    // Log the error to an error reporting service
     console.error(error);
   }, [error]);
 
@@ -30,7 +29,6 @@ export default function Error({
           </p>
           <button
             onClick={
-              // Attempt to recover by trying to re-render the segment
               () => reset()
             }
             className="inline-flex text-white bg-primary-600 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4"
--- src/app/(Customer)/browse/layout.tsx
+++ src/app/(Customer)/browse/layout.tsx
@@ -1,5 +1,3 @@
-import { ChevronLeftIcon } from "@heroicons/react/24/solid";
-import Back from "../components/back";
 import { ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
