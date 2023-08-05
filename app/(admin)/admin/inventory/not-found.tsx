be584ccbb79c4d12a31bc11498effa873aa004f9
faizanibneadil <107530101+faizanibneadil@users.noreply.github.com>
2023-08-05 17:57:28 +0500

    fix - bugs

--- app/(admin)/admin/inventory/not-found.tsx
+++ app/(admin)/admin/inventory/not-found.tsx
@@ -1,14 +1,14 @@
 import { Callout } from "@tremor/react";
-// import InitializeNewCategory from "./_components/initialize-new-category";
-// import RefreshPage from "./_components/refresh-button";
+import InitializeNewInventory from "./_components/initialize-new-inventory";
+import RefreshPage from "./_components/refresh-button";
 
 export default function NotFound() {
   return (
     <div>
-      {/* <div className="flex justify-end mb-4 space-x-2">
-        <InitializeNewCategory />
+      <div className="flex justify-end mb-4 space-x-2">
+        <InitializeNewInventory />
         <RefreshPage />
-      </div> */}
+      </div>
 
       <Callout className="w-full" title="Products not found." color="rose">
         Products are not found. Please create click on Create Product button.
