00cde3b0f10755f398bd6f2363ef0979874b6ed1
faizanibneadil <107530101+faizanibneadil@users.noreply.github.com>
2023-11-02 09:16:01 +0500

    fix - auth error

--- app/(customers)/page.tsx
+++ app/(customers)/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 // import Carousel from "./_components/carousel";
-import { Suspense, memo, use } from "react";
+import { memo, use } from "react";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { Card } from "@/components/ui/card";
 import Link from "next/link";
@@ -23,46 +23,42 @@ const Page: React.FC<Props> = memo(() => {
   const categories = use(_getCategoriesProducts());
   return (
     <div>
-      <Suspense fallback={<div>Loading ...</div>}>
-        <Carousel />
-      </Suspense>
-      <Suspense fallback={<div>Loading ...</div>}>
-        <ScrollArea className="w-full h-auto p-2 pb-4 mt-2 mb-2">
-          <ScrollBar orientation="horizontal" />
-          <div className="flex flex-col space-y-2">
-            <div className="flex flex-row items-center justify-center space-x-2">
-              {categories?.map((category) => (
-                <Link key={category.id} href={`/categories/${category.slug}`}>
-                  <Card className="relative w-20 h-20">
-                    <Image
-                      fill
-                      sizes="100vw"
-                      src={`https://lh3.googleusercontent.com/d/${category?.images?.src}=s220`}
-                      alt=""
-                      className="object-cover w-full h-20 rounded-md"
-                    />
-                  </Card>
-                </Link>
-              ))}
-            </div>
-            <div className="flex flex-row items-center justify-center space-x-2">
-              {categories?.map((category) => (
-                <Link key={category.id} href={`/categories/${category.slug}`}>
-                  <Card className="relative w-20 h-20">
-                    <Image
-                      fill
-                      sizes="100vw"
-                      src={`https://lh3.googleusercontent.com/d/${category?.images?.src}=s220`}
-                      alt=""
-                      className="object-cover w-full h-20 rounded-md"
-                    />
-                  </Card>
-                </Link>
-              ))}
-            </div>
+      <Carousel />
+      <ScrollArea className="w-full h-auto p-2 pb-4 mt-2 mb-2">
+        <ScrollBar orientation="horizontal" />
+        <div className="flex flex-col space-y-2">
+          <div className="flex flex-row items-center justify-center space-x-2">
+            {categories?.map((category) => (
+              <Link key={category.id} href={`/categories/${category.slug}`}>
+                <Card className="relative w-20 h-20">
+                  <Image
+                    fill
+                    sizes="100vw"
+                    src={`https://lh3.googleusercontent.com/d/${category?.images?.src}=s220`}
+                    alt=""
+                    className="object-cover w-full h-20 rounded-md"
+                  />
+                </Card>
+              </Link>
+            ))}
           </div>
-        </ScrollArea>
-      </Suspense>
+          <div className="flex flex-row items-center justify-center space-x-2">
+            {categories?.map((category) => (
+              <Link key={category.id} href={`/categories/${category.slug}`}>
+                <Card className="relative w-20 h-20">
+                  <Image
+                    fill
+                    sizes="100vw"
+                    src={`https://lh3.googleusercontent.com/d/${category?.images?.src}=s220`}
+                    alt=""
+                    className="object-cover w-full h-20 rounded-md"
+                  />
+                </Card>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </ScrollArea>
       <div>
         {categories?.map((category) => (
           <div key={category.id} className="flex flex-col space-y-2">
@@ -72,44 +68,42 @@ const Page: React.FC<Props> = memo(() => {
                 <ArrowRightIcon />
               </Link>
             </div>
-            <Suspense fallback={<div>Loading ...</div>}>
-              <ScrollArea className="w-full h-auto p-2 pb-4 mt-2 mb-2">
-                <ScrollBar orientation="horizontal" />
-                <div className="flex flex-row items-center justify-center space-x-2">
-                  {category.Products?.map((item) => (
-                    <Link
-                      key={item.id}
-                      href={`/view/${item.slug}`}
-                      className="flex flex-col w-40 space-y-1"
-                    >
-                      <Card className="relative w-40 h-32">
-                        <Image
-                          fill
-                          sizes="100vw"
-                          src={`https://lh3.googleusercontent.com/d/${item?.images[0]?.src}=s220`}
-                          alt=""
-                          className="object-cover w-full h-20 mb-2 rounded-md"
-                        />
-                        {item?.discountInPercentage > 0 && (
-                          <Badge
-                            className="absolute bottom-1 left-1"
-                            variant="destructive"
-                          >
-                            {`${item?.discountInPercentage?.toFixed()}% OFF`}
-                          </Badge>
-                        )}
-                      </Card>
-                      <h2 className="text-sm line-clamp-2">{item.title}</h2>
-                      <div className="flex flex-row items-center justify-between">
-                        <Badge className="">
-                          {priceFormatter.format(Number(item.salePrice))}
+            <ScrollArea className="w-full h-auto p-2 pb-4 mt-2 mb-2">
+              <ScrollBar orientation="horizontal" />
+              <div className="flex flex-row items-center justify-center space-x-2">
+                {category.Products?.map((item) => (
+                  <Link
+                    key={item.id}
+                    href={`/view/${item.slug}`}
+                    className="flex flex-col w-40 space-y-1"
+                  >
+                    <Card className="relative w-40 h-32">
+                      <Image
+                        fill
+                        sizes="100vw"
+                        src={`https://lh3.googleusercontent.com/d/${item?.images[0]?.src}=s220`}
+                        alt=""
+                        className="object-cover w-full h-20 mb-2 rounded-md"
+                      />
+                      {item?.discountInPercentage > 0 && (
+                        <Badge
+                          className="absolute bottom-1 left-1"
+                          variant="destructive"
+                        >
+                          {`${item?.discountInPercentage?.toFixed()}% OFF`}
                         </Badge>
-                      </div>
-                    </Link>
-                  ))}
-                </div>
-              </ScrollArea>
-            </Suspense>
+                      )}
+                    </Card>
+                    <h2 className="text-sm line-clamp-2">{item.title}</h2>
+                    <div className="flex flex-row items-center justify-between">
+                      <Badge className="">
+                        {priceFormatter.format(Number(item.salePrice))}
+                      </Badge>
+                    </div>
+                  </Link>
+                ))}
+              </div>
+            </ScrollArea>
           </div>
         ))}
       </div>
